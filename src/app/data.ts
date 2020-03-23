14c3683519e23b76af3b0758ca1f15716f51e79b
arcsinista64 <ing.garciaruiz.9707@gmail.com>
2020-03-23 11:36:02 -0600

    Add models & data

---
+++ src/app/data.ts
@@ -0,0 +1,49 @@
+import { Automovil } from "./models";
+
+
+export var AUTOMOVILES: Array<Automovil> =
+[
+    {
+        marca:  "Tesla",
+        submarca: "Cybertruck",
+        modelo: 2019,
+        descripcion: "Tesla Cybertruck Gris",
+        fecha_registro: new Date('2020-03-10')
+    },
+    {
+        marca:  "Tesla",
+        submarca: "Model 3",
+        modelo: 2016,
+        descripcion: "Tesla Model 3 Azul",
+        fecha_registro: new Date('2020-03-10')
+    },
+    {
+        marca:  "Tesla",
+        submarca: "Model S",
+        modelo: 2017,
+        descripcion: "Tesla Model S Negro",
+        fecha_registro: new Date('2020-03-10')
+    },
+    {
+        marca:  "Tesla",
+        submarca: "Model X",
+        modelo: 2015,
+        descripcion: "Tesla Model X Blanco",
+        fecha_registro: new Date('2020-03-10')
+    },
+    {
+        marca:  "Tesla",
+        submarca: "Model Y",
+        modelo: 2016,
+        descripcion: "Tesla Model Y Gris",
+        fecha_registro: new Date('2020-03-10')
+    },
+    {
+        marca:  "Tesla",
+        submarca: "Roadster",
+        modelo: 2017,
+        descripcion: "Tesla Roadster Rojo",
+        fecha_registro: new Date('2020-03-10')
+
+    },
+];
